d8b57272114cc32120e77efbd91391b79c874528
Попов Владислав <vpopov@gmail.com>
2024-10-24 17:47:06 +0500

    Change gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -24,18 +24,44 @@ function reloadJs() {
         .pipe(browserSync.stream());
 }
 
+
+// Копирование PNG изображений
+function processPngImages() {
+    return src('src/images/*.png')
+        .pipe(dest('dist/images'))
+        .pipe(browserSync.stream());
+}
+
+// Копирование SVG изображений
+function processSvgImages() {
+    return src('src/images/*.svg')
+        .pipe(dest('dist/images'))
+        .on('end', () => console.log('PNG images processed'))
+        .pipe(browserSync.stream());
+}
+
+// Копирование WOFF2 шрифтов
+function processFonts() {
+    return src('src/fonts/*.woff2')
+        .pipe(dest('dist/fonts'))
+        .pipe(browserSync.stream());
+}
+
 // Инициализация сервера
 function serve() {
     browserSync.init({
         server: {
             baseDir: 'dist'
-        }
+        },
     });
 
     // Наблюдение за изменениями файлов
     watch('src/styles/*.scss', compileSass);
     watch('src/*.html', reloadHtml);
     watch('src/scripts/*.js', reloadJs);
+    watch('src/images/*.png', processPngImages);
+    watch('src/images/*.svg', processSvgImages);
+    watch('src/images/*.woff2', processFonts);
 }
 
-exports.default = series(compileSass, reloadHtml, reloadJs, serve);
+exports.default = series(compileSass, reloadHtml, reloadJs, processPngImages, processSvgImages, processFonts, serve);
